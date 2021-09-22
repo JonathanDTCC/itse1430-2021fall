d75a922d947841c7411355d24cb2593887923c3e
ZachArtHunter <38150080+ZachArtHunter@users.noreply.github.com>
2021-09-21 19:37:25 -0500

    Lab1 - Story 1

---
+++ labs/Lab1/JonaDaniel.AdventureGame.ConsoleHost/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Jonathan Daniel
+ * ITSE 1430 Fall 2021
+ */
+using System;
+
+namespace JonaDaniel.AdventureGame.ConsoleHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
