82f33940b5cb6bc49101ec89f7e8133b90ccf624
ZachArtHunter <38150080+ZachArtHunter@users.noreply.github.com>
2021-09-21 19:58:16 -0500

    Lab1 - Story 2

--- labs/Lab1/JonaDaniel.AdventureGame.ConsoleHost/Program.cs
+++ labs/Lab1/JonaDaniel.AdventureGame.ConsoleHost/Program.cs
@@ -10,7 +10,24 @@ namespace JonaDaniel.AdventureGame.ConsoleHost
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DisplayIntroduction();
+        }
+        static void DisplayIntroduction()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("ITSE 1430 Adventure Game Fall 2021");
+            Console.WriteLine("".PadRight(15, '-'));
+            Console.WriteLine("");
+            Console.ResetColor();
+
+            //TODO: Make better introduction once an area has been planned and established later
+            Console.WriteLine("Why Sentence");
+            Console.WriteLine("Where Sentence");
+            Console.WriteLine("What next Sentence");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("What do you want to do?");
+            Console.ResetColor();
         }
     }
 }
