c5ca10e8bd6ad7ab4b78ea8ed92d28e7031e4d3c
JonathanDTCC <jonathan.daniel749@my.tccd.edu>
2021-10-11 21:39:43 -0500

    Monday 10-11-2021 Classwork -Controls -Child Forms

--- classwork/MovieLibrary/MovieLibrary.WinHost/MainForm.cs
+++ classwork/MovieLibrary/MovieLibrary.WinHost/MainForm.cs
@@ -12,5 +12,40 @@ namespace MovieLibrary.WinHost
             //Additional init here
             //Runs at design time as well - be careful
         }
+
+        private void OnFileExit ( object sender, EventArgs e )
+        {
+            //Confirm exit?
+            if (!Confirm("Do you want to quit?", "Confirm"))
+                return;
+
+            Close();
+        }
+
+        private static bool Confirm ( string message, string title )
+        {
+            return MessageBox.Show(message, title,
+                                   MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+                        == DialogResult.Yes;
+        }
+
+        private void OnHelpAbout ( object sender, EventArgs e )
+        {
+            var dlg = new AboutBox();
+
+            //Blocks until child form is closed
+            dlg.ShowDialog();
+
+            //Show displays modeless, not blocking
+            //dlg.Show();
+            //MessageBox.Show("After Show");
+        }
+
+        private void OnMovieAdd ( object sender, EventArgs e )
+        {
+            var dlg = new MovieForm();
+
+            dlg.ShowDialog();
+        }
     }
 }
---
+++ classwork/MovieLibrary/MovieLibrary.WinHost/MovieForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovieLibrary.WinHost
+{
+    public partial class MovieForm : Form
+    {
+        public MovieForm ()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSave ( object sender, EventArgs e )
+        {
+            //Build up a Movie
+            var movie = new Movie();
+            movie.Title = _txtTitle.Text;
+            movie.Description = _txtDescription.Text;
+            movie.Rating = _cbRating.SelectedText;
+
+            //TODO: Validate
+
+            //TODO: Return movie
+
+            //Close the form
+            Close();
+        }
+    }
+}
