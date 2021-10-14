dc6b0e7107104d93a9294e213eb02e72bed091bc
JonathanDTCC <jonathan.daniel749@my.tccd.edu>
2021-10-13 21:14:52 -0500

    Wednesday 10-12-2021 (Classwork) -Controls 2 -Child Forms 2 -Events

--- classwork/MovieLibrary/MovieLibrary.WinHost/MainForm.cs
+++ classwork/MovieLibrary/MovieLibrary.WinHost/MainForm.cs
@@ -45,7 +45,60 @@ namespace MovieLibrary.WinHost
         {
             var dlg = new MovieForm();
 
-            dlg.ShowDialog();
+            //ShowDialog -> DialogResult
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+
+            //TODO: Save movie
+            _movie = dlg.Movie;
+            UpdateUI();
+        }
+
+        private void OnMovieEdit ( object sender, EventArgs e )
+        {
+            if (_movie == null)
+                return;
+
+            var dlg = new MovieForm();
+            dlg.Movie = _movie;
+
+            //ShowDialog -> DialogResult
+            if (dlg.ShowDialog() != DialogResult.OK)
+                return;
+
+            //TODO: Save movie
+            _movie = dlg.Movie;
+            UpdateUI();
+        }
+
+        private Movie _movie;
+
+        private void UpdateUI ()
+        {
+            //Update movie list
+            var movies = (_movie != null) ? new Movie[1] : new Movie[0];
+            if(_movie != null)
+                movies[0] =_movie;
+
+            var bindingSource = new BindingSource();
+            bindingSource.DataSource = movies;
+
+            //bind the movies to the listbox
+            _listMovies.DataSource = bindingSource;
+        }
+
+        private void OnMovieDelete ( object sender, EventArgs e )
+        {
+            if (_movie == null)
+                return;
+
+            //Confirmation
+            if (!Confirm($"Are you sure you want to delete '{_movie.Title}'?", "Delete"))
+                return;
+
+            //TODO: Delete
+            _movie = null;
+            UpdateUI();
         }
     }
 }
--- classwork/MovieLibrary/MovieLibrary.WinHost/MovieForm.cs
+++ classwork/MovieLibrary/MovieLibrary.WinHost/MovieForm.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// ITSE 1430
+// Movie Library
+
+using System;
 using System.Windows.Forms;
 
 namespace MovieLibrary.WinHost
@@ -17,6 +13,28 @@ namespace MovieLibrary.WinHost
             InitializeComponent();
         }
 
+        public Movie Movie { get; set; }
+
+        protected override void OnLoad ( EventArgs e )
+        {
+            //Always call base version first
+            base.OnLoad(e);
+
+            //Load UI, if necessary
+            if (Movie != null)
+                LoadMovie(Movie);
+        }
+
+        private void LoadMovie( Movie movie )
+        {
+            _txtTitle.Text = movie.Title;
+            _txtDescription.Text = movie.Description;
+            _cbRating.Text = movie.Rating;
+            _txtRunLength.Text = movie.RunLength.ToString();
+            _txtReleaseYear.Text = movie.ReleaseYear.ToString();
+            _chkIsClassic.Checked = movie.IsClassic;
+        }
+
         private void OnSave ( object sender, EventArgs e )
         {
             //Build up a Movie
@@ -24,13 +42,39 @@ namespace MovieLibrary.WinHost
             movie.Title = _txtTitle.Text;
             movie.Description = _txtDescription.Text;
             movie.Rating = _cbRating.SelectedText;
+            movie.RunLength = GetInt32(_txtRunLength);
+            movie.ReleaseYear = GetInt32(_txtReleaseYear);
+            movie.IsClassic = _chkIsClassic.Checked;
 
-            //TODO: Validate
+            //Validate
+            var error = movie.Validate();
+            if (!String.IsNullOrEmpty(error))
+            {
+                DisplayError(error, "Error");
+                DialogResult = DialogResult.None;
+                return;
+            };
+
+            Movie = movie;
 
             //TODO: Return movie
 
             //Close the form
-            Close();
+            //Close();
+        }
+
+        private void DisplayError ( string message, string title )
+        {
+            MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private int GetInt32 ( Control /*TextBox*/ control )
+        {
+            var text = control.Text;
+            if (Int32.TryParse(text, out var result))
+                return result;
+
+            return -1;
         }
     }
 }
