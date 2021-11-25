c0603a2fc09ed9907ecb61b844a828c013abbdb2
ZachArtHunter <38150080+ZachArtHunter@users.noreply.github.com>
2021-11-25 17:01:12 -0600

    Lab 1 - Story 4

--- labs/Lab1/JonaDaniel.AdventureGame.ConsoleHost/Program.cs
+++ labs/Lab1/JonaDaniel.AdventureGame.ConsoleHost/Program.cs
@@ -20,7 +20,8 @@ namespace JonaDaniel.AdventureGame.ConsoleHost
 
                 switch(command)
                 {
-                    case "quit" done = HandleQuit(); break;
+                    case "quit": done = HandleQuit(); break;
+                    case "help": HelpList(); break;
                     case "look": SearchArea(); break;
                     case "move":
                     case "movenorth": MoveNorth(); break;
@@ -31,6 +32,46 @@ namespace JonaDaniel.AdventureGame.ConsoleHost
                 };
             } while (!done);
         }
+        #region Display Methods
+        static void DisplayIntroduction ()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("ITSE 1430 Adventure Game Fall 2021");
+            Console.WriteLine("".PadRight(15, '-'));
+            Console.WriteLine("");
+            Console.ResetColor();
+
+            //TODO: Make better introduction once an area has been planned and established later
+            Console.WriteLine("Why Sentence");
+            Console.WriteLine("Where Sentence");
+            Console.WriteLine("What next Sentence");
+
+            //Console.ForegroundColor = ConsoleColor.Green;
+            //Console.WriteLine("What do you want to do?");
+            //Console.ResetColor();
+        }
+        static void HelpList ()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("");
+            Console.WriteLine("Move");
+            Console.WriteLine("MoveNorth");
+            Console.WriteLine("MoveSouth");
+            Console.WriteLine("MoveEast");
+            Console.WriteLine("MoveWest");
+            Console.WriteLine("Look");
+            Console.WriteLine("Quit");
+            Console.ResetColor();
+        }
+        static void DisplayError ( string message )
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        #endregion
+
+        #region Game Commands
         static void SearchArea()
         {
             //TODO: Later
@@ -52,36 +93,35 @@ namespace JonaDaniel.AdventureGame.ConsoleHost
         {
             Console.WriteLine("Move West");
         }
-        static void DisplayIntroduction()
+
+        #endregion
+
+        #region UserInputHandling
+        static bool HandleQuit ()
         {
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("ITSE 1430 Adventure Game Fall 2021");
-            Console.WriteLine("".PadRight(15, '-'));
-            Console.WriteLine("");
-            Console.ResetColor();
-
-            //TODO: Make better introduction once an area has been planned and established later
-            Console.WriteLine("Why Sentence");
-            Console.WriteLine("Where Sentence");
-            Console.WriteLine("What next Sentence");
-
-            //Console.ForegroundColor = ConsoleColor.Green;
-            //Console.WriteLine("What do you want to do?");
-            //Console.ResetColor();
+            return ReadBoolean("Are you sure you want to quit? Y/N");
         }
-        static void DisplayError( string message )
+        static bool ReadBoolean ( string message )
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(message);
-            Console.ResetColor();
+            Console.Write(message);
+            do
+            {
+                ConsoleKeyInfo input = Console.ReadKey(true);
+                if (input.Key == ConsoleKey.Y)
+                    return true;
+                else if (input.Key == ConsoleKey.N)
+                    return false;
+            } while (true);
         }
         static string GetInput()
         {            
             while (true)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("What do you want to do?");
+                Console.WriteLine("");
+                Console.Write("What do you want to do? ");
                 Console.ResetColor();
+
                 string input = Console.ReadLine().Trim();
 
                 switch (input.ToUpper())
@@ -92,10 +132,15 @@ namespace JonaDaniel.AdventureGame.ConsoleHost
                     case "MOVEEAST": return "moveeast";
                     case "MOVEWEST": return "movewest";
                     case "LOOK": return "look";
+                    case "HELP": return "help";
+                    case "QUIT": return "quit";
                 };
 
                 DisplayError("I don't know what that means");
+                Console.WriteLine("Type 'help' for a list of commands");
+                Console.WriteLine("");
             }
         }
+        #endregion
     }
 }
