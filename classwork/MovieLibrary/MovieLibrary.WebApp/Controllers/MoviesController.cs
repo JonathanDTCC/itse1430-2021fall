6052bda4422aa477fdda442aa8ba71bf033f0160
JonathanDTCC <jonathan.daniel749@my.tccd.edu>
2021-12-06 21:23:15 -0600

    12-6-2021 Classwork -Webapps -Model -View -Controller

---
+++ classwork/MovieLibrary/MovieLibrary.WebApp/Controllers/MoviesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieLibrary.WebApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public MoviesController( IMovieDatabase database )
+        {
+            _database = database;
+        }
+
+        public IActionResult List ()
+        {
+            var movies = _database.GetAll()
+                                    .OrderBy(x => x.Title);
+
+            return View(movies);
+            //return Json(new Movie() {
+            //    Title = "Jaws",
+            //    Description = "Shark movie",
+            //    ReleaseYear = 1978,
+            //    RunLength = 210,
+            //});
+        }
+
+        private readonly IMovieDatabase _database;
+    }
+}
