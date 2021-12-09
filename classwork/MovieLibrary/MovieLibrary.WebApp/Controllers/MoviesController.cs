7a2e3768510d68d30960e72ce29a319769162d97
JonathanDTCC <jonathan.daniel749@my.tccd.edu>
2021-12-08 21:24:51 -0600

    12-8-2021 Classwork Wednesday -MVC -Routes -Models -Views

--- classwork/MovieLibrary/MovieLibrary.WebApp/Controllers/MoviesController.cs
+++ classwork/MovieLibrary/MovieLibrary.WebApp/Controllers/MoviesController.cs
@@ -9,7 +9,7 @@ namespace MovieLibrary.WebApp.Controllers
 {
     public class MoviesController : Controller
     {
-        public MoviesController( IMovieDatabase database )
+        public MoviesController ( IMovieDatabase database )
         {
             _database = database;
         }
@@ -27,6 +27,97 @@ namespace MovieLibrary.WebApp.Controllers
             //    RunLength = 210,
             //});
         }
+
+        #region Edit GP
+        [HttpGet]
+        public IActionResult Edit ( int id )
+        {
+            var movie = _database.Get(id);
+            if (movie == null)
+                return NotFound();
+
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Edit ( int id, Movie movie )
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _database.Update(id, movie);
+
+                    //PRG - post, redirect (to list), get
+                    return RedirectToAction(nameof(List));
+                };
+            } catch (Exception e)
+            {
+                ModelState.AddModelError("", e.Message);
+            };
+
+            return View(movie);
+        }
+        #endregion
+
+        #region Create GP
+        [HttpGet]
+        public IActionResult Create ()
+        {
+            var movie = new Movie();
+
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Create ( Movie movie )
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _database.Add(movie);
+
+                    //PRG - post, redirect (to list), get
+                    return RedirectToAction(nameof(List));
+                };
+            } catch (Exception e)
+            {
+                ModelState.AddModelError("", e.Message);
+            };
+
+            return View(movie);
+        }
+        #endregion
+
+        #region Delete GP
+        [HttpGet]
+        public IActionResult Delete ( int id )
+        {
+            var movie = _database.Get(id);
+            if (movie == null)
+                return NotFound();
+
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Delete ( int id, Movie movie )
+        {
+            try
+            {
+                _database.Delete(id);
+
+                //PRG - post, redirect (to list), get
+                return RedirectToAction(nameof(List));
+            } catch (Exception e)
+            {
+                ModelState.AddModelError("", e.Message);
+            };
+
+            return View(movie);
+        }
+        #endregion
 
         private readonly IMovieDatabase _database;
     }
